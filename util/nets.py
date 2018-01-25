7de9e8cd779315120f10197b7bbc0930dc961aff
Mitiku <se.mitiku.yohannes@gmail.com>
2018-01-25 16:36:39 +0300

    Added custom model checkpoint

--- util/nets.py
+++ util/nets.py
@@ -42,7 +42,50 @@ class LambdaUpdateCallBack(keras.callbacks.Callback):
         if LAMDA<1:
             LAMDA +=5e-6
         return
+class CustomModelCheckPoint(keras.callbacks.Callback):
+    def __init__(self,**kargs):
+        callbacks.Callback.__init__(self,**kargs)
+        self.last_loss = 1000000000
+        self.last_accuracy = 0
+        self.current_model_number = 0;
+        self.epoch_number = 0
+    # def on_train_begin(self,epoch, logs={}):
+    #     return
+ 
+    # def on_train_end(self, logs={}):
+    #     return
+ 
+    def on_epoch_begin(self,epoch, logs={}):
+        return
 
+    def on_epoch_end(self, epoch, logs={}):
+        self.epoch_number+=1
+        current_loss = logs.get("val_loss")
+        current_accuracy  = logs.get("val_acc")
+        if (self.last_loss-current_loss) > 0.01:
+            current_weights_name = "weights"+str(self.current_model_number)+".h5"
+            print("loss improved from "+str(self.last_loss)+" to "+str(current_loss)+", Saving model to "+current_weights_name)
+            self.model.save_weights("models/"+current_weights_name);
+            self.model.save_weights("models/last_weight.h5")
+            self.current_model_number+=1
+            self.last_loss = current_loss
+            self.last_accuracy = current_accuracy
+            with open("log.txt","a+") as logfile:
+                logfile.write("________________________________________________________\n")
+                logfile.write("EPOCH    =")
+                logfile.write(str(epoch)+"\n")
+                logfile.write("VAL_ACC  =")
+                logfile.write(str(current_accuracy)+"\n")
+                logfile.write("VAL_LOSS =")
+                logfile.write(str(current_loss)+"\n")
+                logfile.write("ACCURACY =")
+                logfile.write(str(logs.get("acc"))+"\n")
+                logfile.write("LOSS     =")
+                logfile.write(str(logs.get("loss"))+"\n")
+                logfile.write("********************************************************\n")
+            with open("epoch_number.json","w+") as json_file:
+                data = {"epoch_number":self.epoch_number}
+                json.dump(data,json_file,indent=4)
     
 
 class AllInOneNeuralNetwork(object):
@@ -232,12 +275,36 @@ class AllInOneNeuralNetwork(object):
                 print "Unable to load model from ",model_path," model path does not exist"
         agModel = self.get_age_gender_model()
         agModel.summary()
-        
+        ### Resume
+        customCheckPoint = CustomModelCheckPoint()
+        REMAINING_EPOCHS = self.epochs
+        if (self.resume):
+            if os.path.exists("epoch_number.json"):
+                with open("epoch_number.json","r") as json_file:
+                    try:
+                        data = json.load(json_file)
+                        customCheckPoint.epoch_number = data["epoch_number"]
+                        REMAINING_EPOCHS -= customCheckPoint.epoch_number
+                    except:
+
+                        print ("unable to read epoch number from file. resuming epoch from 0.")
+
+                    
+                print("resuming from previous epoch number:")
+                print("previous epoch number",customCheckPoint.epoch_number)
+                print("remaining epochs",REMAINING_EPOCHS)
+        if REMAINING_EPOCHS < 0:
+            REMAINING_EPOCHS =1
+        with open("log.txt","a+") as logfile:
+            str_date = datetime.now().strftime("%d, %b, %Y %H:%M:%S")
+            logfile.write("Starting to train model\n")
+            logfile.write(str_date+"\n")
+        ### Resume end
         X_test, age_test,gender = self.imdb_preprocessor.test_dataset
         gender_test = np.eye(2)[gender]
         y_test = [age_test,gender_test]
         
-        agModel.fit_generator(self.imdb_preprocessor.generator(batch_size=self.batch_size),epochs = self.epochs,callbacks = [LambdaUpdateCallBack()],steps_per_epoch=1000,validation_data=(X_test,y_test),verbose=True)
+        agModel.fit_generator(self.imdb_preprocessor.generator(batch_size=self.batch_size),epochs = self.epochs,callbacks = [LambdaUpdateCallBack(),customCheckPoint],steps_per_epoch=1000,validation_data=(X_test,y_test),verbose=True)
         with open("logs/logs.txt","a+") as log_file:
             score = agModel.evaluate(X_test,y_test)
             log_file.write(str(score))
