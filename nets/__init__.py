0ef79c4fd3e652548af022ac9c474991c7929ba0
Mitiku <se.mitiku.yohannes@gmail.com>
2018-02-05 11:35:51 +0300

    Changed age loss weight

--- nets/__init__.py
+++ nets/__init__.py
@@ -320,7 +320,7 @@ class AllInOneNetwork(object):
             self.dataset.load_dataset()
         assert self.dataset.dataset_loaded ==True, "Dataset is not loaded"
         ageGenderModel = self.get_model_with_labels(["age_estimation","gender_probablity"])
-        ageGenderModel.compile(loss = [age_margin_mse_loss, keras.losses.categorical_crossentropy],loss_weights=[0.9,1],optimizer=keras.optimizers.Adam(self.learning_rate),metrics=["accuracy"])
+        ageGenderModel.compile(loss = [age_margin_mse_loss, keras.losses.categorical_crossentropy],loss_weights=[0.5,1],optimizer=keras.optimizers.Adam(self.learning_rate),metrics=["accuracy"])
         ageGenderModel.summary()
 
         X_test = self.dataset.test_dataset_images
