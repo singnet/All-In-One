f465999180facea5673de89d725f53d57e9476cf
Mitiku <se.mitiku.yohannes@gmail.com>
2018-02-05 11:18:40 +0300

    Changed age loss to margin mse loss

--- nets/__init__.py
+++ nets/__init__.py
@@ -36,6 +36,8 @@ class RoundLayer(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+def age_margin_mse_loss(y_true,y_pred):
+    return max(K.square(y_pred -y_true)-2.25,0)
 
 def age_loss(y_true,y_pred):
     global LAMDA,SIGMOID
@@ -318,19 +320,19 @@ class AllInOneNetwork(object):
             self.dataset.load_dataset()
         assert self.dataset.dataset_loaded ==True, "Dataset is not loaded"
         ageGenderModel = self.get_model_with_labels(["age_estimation","gender_probablity"])
-        ageGenderModel.compile(loss = [age_loss, keras.losses.categorical_crossentropy],loss_weights=[0.9,1],optimizer=keras.optimizers.Adam(self.learning_rate),metrics=["accuracy"])
+        ageGenderModel.compile(loss = [age_margin_mse_loss, keras.losses.categorical_crossentropy],loss_weights=[0.9,1],optimizer=keras.optimizers.Adam(self.learning_rate),metrics=["accuracy"])
         ageGenderModel.summary()
 
         X_test = self.dataset.test_dataset_images
-        age_test = self.dataset.test_dataset["Age"].as_matrix().astype(np.float32)/150.0
+        age_test = self.dataset.test_dataset["Age"].as_matrix()
         gender_test = self.dataset.test_dataset["Gender"].as_matrix().astype(np.uint8)
         gender_test = np.eye(2)[gender_test]
         y_test = [age_test,gender_test]
         if self.resume:
             checkPoint = self.resume_model()
-            callbacks = [checkPoint,LambdaUpdateCallBack()]
+            callbacks = [checkPoint]
         else:
-            callbacks = [CustomModelCheckPoint(),LambdaUpdateCallBack()]
+            callbacks = [CustomModelCheckPoint()]
         ageGenderModel.fit_generator(self.dataset.generator(batch_size=self.batch_size),epochs = self.epochs,callbacks = callbacks,steps_per_epoch=self.steps_per_epoch,validation_data=(X_test,y_test),verbose=True)
         with open("logs/logs.txt","a+") as log_file:
             score = ageGenderModel.evaluate(X_test,y_test)
