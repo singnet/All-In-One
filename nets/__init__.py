529f55498bd5ff11f8761de39f0d094003ff16fe
Mitiku <se.mitiku.yohannes@gmail.com>
2018-02-14 10:54:57 +0300

    Some random changes

--- nets/__init__.py
+++ nets/__init__.py
@@ -182,7 +182,7 @@ class AllInOneNetwork(object):
         smile_model.compile(loss = age_loss,optimizer=keras.optimizers.Adam(self.config.getLearningRate()),metrics=["accuracy"])
         callbacks = [LambdaUpdateCallBack()]
         smile_model.summary()
-        smile_model.fit_generator(dataset.age_data_genenerator(self.config.batch_size),
+        smile_model.fit_generator(dataset.smile_data_generator(self.config.batch_size),
                 epochs = self.config.epochs,
                 steps_per_epoch = self.config.steps_per_epoch,
                 validation_data = [X_test,smile_test],
